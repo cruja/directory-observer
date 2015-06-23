474c97414198b925defa5678cb458d2c1937f56e
Iulian Balan <iulian.balan@hp.com>
2015-06-23 07:27:25 +0300

    initial commit

---
+++ src/main/java/DirectoryAccessException.java
@@ -0,0 +1,16 @@
+
+public class DirectoryAccessException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DirectoryAccessException(String msg) {
+		super(msg);
+	}
+	public DirectoryAccessException() {
+		super();
+	}
+
+}
---
+++ src/main/java/QueueHandler.java
@@ -0,0 +1,30 @@
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.BlockingQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QueueHandler implements Observer{
+	
+	private static final Logger log = LoggerFactory.getLogger(QueueHandler.class);
+	
+	private BlockingQueue<MessagePojo> testQueue;
+	
+	public QueueHandler(BlockingQueue<MessagePojo> queue) {
+		this.testQueue = queue;
+	}
+
+	public void update(Observable o, Object arg) {
+		if (arg instanceof MessagePojo) {
+			MessagePojo msg = (MessagePojo)arg;
+			log.debug(String.format("The queue handler was informed that the file '%s' was %s", msg.getFile(), msg.getAction()));
+			try {
+				testQueue.put(msg);
+			} catch (InterruptedException e) {
+				log.debug(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+}
